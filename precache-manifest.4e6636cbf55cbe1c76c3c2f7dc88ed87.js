e897b536de0201f82e21332559d5a40cbfca834f
MaxOzymok <ozymokmax1@ukr.net>
2020-04-06 02:18:15 +0300

    Updates

---
+++ precache-manifest.4e6636cbf55cbe1c76c3c2f7dc88ed87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e844cdcba75d0b38d3732c48c81b6c",
+    "url": "/CursorHW_18/index.html"
+  },
+  {
+    "revision": "66195775bb8fae3b23a8",
+    "url": "/CursorHW_18/static/css/main.8f84594c.chunk.css"
+  },
+  {
+    "revision": "10ddf8a950a3217cf2a6",
+    "url": "/CursorHW_18/static/js/2.948a96b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CursorHW_18/static/js/2.948a96b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66195775bb8fae3b23a8",
+    "url": "/CursorHW_18/static/js/main.b83a1db0.chunk.js"
+  },
+  {
+    "revision": "a329b6d3e0b421743809",
+    "url": "/CursorHW_18/static/js/runtime-main.b71ee5d1.js"
+  }
+]);
